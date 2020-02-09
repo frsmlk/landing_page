7257829d607cf069de4f1a1777796df59f4d1928
Faris <mfarismalik@gmail.com>
2020-02-09 08:57:46 +0800

    font applied

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
       <header className="App-header"  >
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          We have served 5140 meals to 91 charity homes.
+          We have served <strong>4421</strong> meals to <strong>95</strong> charity homes.
         </p>
 
         <p stlye={{ fontSize: '8px' }}>
@@ -32,18 +32,18 @@ function App() {
           <a href='https://www.instagram.com/ao.socialgiving'>
             <img className="img" alt="instagram" src={instagram} />
           </a>
-          <p>ao.socialgiving</p>
+          <p><strong>ao.socialgiving</strong></p>
         </div>
         <div class="col">
           <a href='http://www.wasap.my/+60186660826'>
             <img className="img" alt="whatsapp" src={whatsapp} />
           </a>
-          <p>018 666 826</p>
+          <p><strong>018 666 826</strong></p>
         </div>
-        <div class="col"> <a href='https://www.gmail.com'>
+        <div class="col"> <a href='mailto:hello@arisoffering.com'>
           <img className="img" alt="gmail" src={gmail} />
         </a>
-          <p>hello@arisoffering.com</p>
+          <p><strong>hello@arisoffering.com</strong></p>
         </div>
 
       </div>
